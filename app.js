7d2c77c061771e4b335758e0da43c11fbaae9a9c
Thomas <Thomasneldeberg@gmail.com>
2017-10-26 10:56:29 +0200

    Test af abcens list - json data

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 (function () {
+
     var app = angular.module('absenceList', []);
-    
-    app.controller('StudentController', function($scope) {
-        $scope.studentList = students;
-        console.log("inside controlelr " + $scope.studentList);
+
+    app.controller('absenceController', function ($scope, $http) {
+        // Getting the data from the API
+var url="https://gist.githubusercontent.com/planetoftheweb/2c2f3b03b72a7f2ae923/raw/d0236fd0a945fdac7acd463f268bd20ebe4d766c/data.json";
+
+        $http.get(url).then(function (response) {
+            $scope.dataset = response.data;
+
+        });
+
     });
-
-    var students = [
-
-        {
-            name: 'Pipi',
-            absence: false
-        },
-        {
-            name: 'Kim',
-            absence: true
-        },
-        {
-            name: 'Mads',
-            absence: false
-        },
-        {
-            name: 'Jonas',
-            absence: false
-        },
-        {
-            name: 'Mona',
-            absence: true
-        },
-];
-    console.log(students);
 
 })();
