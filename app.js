352b79c502b75f702c88599a0f697deaf37fb61b
Thomas <Thomasneldeberg@gmail.com>
2017-10-26 11:48:00 +0200

    absenslist (not working - json fix)

--- app.js
+++ app.js
@@ -4,13 +4,10 @@
 
     app.controller('absenceController', function ($scope, $http) {
         // Getting the data from the API
-var url="https://gist.githubusercontent.com/planetoftheweb/2c2f3b03b72a7f2ae923/raw/d0236fd0a945fdac7acd463f268bd20ebe4d766c/data.json";
 
-        $http.get(url).then(function (response) {
-            $scope.dataset = response.data;
-
-        });
-
+        $http.get("data.json")
+            .success(function (data) {
+                $scope.name = data.array;
+            })
     });
-
 })();
